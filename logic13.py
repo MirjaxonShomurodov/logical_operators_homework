b63f3801279638354f6ab3fa623cb0e23b1bc594
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 23:20:48 +0500

    13 misol

--- logic13.py
+++ logic13.py
@@ -1,9 +1,11 @@
+
 def main(a):
-    """
+    return a//10 + a%10 and a%2==0 
+print(main(25))
+"""
     Given a two-digit integer a,  check the following statement "All digits sum is even".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
