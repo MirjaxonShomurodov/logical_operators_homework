04f4af3e72eab153d6a7f04259fc051933eb8484
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 23:39:48 +0500

    16 misol

--- logic16.py
+++ logic16.py
@@ -1,9 +1,10 @@
 def main(a):
-    """
+    return a>9999 and a<100000
+print(main(12345))
+"""
     Given integer a,  check the following statement "The integer is a five-digit number".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
