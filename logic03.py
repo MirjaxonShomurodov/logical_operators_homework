575a06b5f534e5e41040c3319bc7e3570ed641e6
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 21:06:07 +0500

    3 misol

--- logic03.py
+++ logic03.py
@@ -1,5 +1,7 @@
 def main(a,b):
-    """
+    return a<0 and b<0
+print(main(-3,-4))
+"""
     Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is negative".
     Args:
         a(int): parameter a
@@ -7,4 +9,3 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
