b650230215243f6ca254a5774876a0e658f49f7c
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 22:55:31 +0500

    10 misol

--- logic10.py
+++ logic10.py
@@ -1,9 +1,10 @@
 def main(a):
-    """
+    return (a>9 and a<100)
+print(main(123))
+"""
     Given integer a,  check the following statement "The integer is two-digit number".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
