49e8e6f8adc3a21eb2ed660e643821284d1660db
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 22:14:37 +0500

    8 misol

--- logic08.py
+++ logic08.py
@@ -1,5 +1,7 @@
 def main(a,b):
-    """
+    return (a%2==0 and b%2==1) or (a%2==1 and b%2==0)
+print(main(7,12))
+"""
     Given two integers a, b,  check the following statement "At least one of the numbers 'a' and 'b' is even".
     Args:
         a(int): parameter a
@@ -7,4 +9,3 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
