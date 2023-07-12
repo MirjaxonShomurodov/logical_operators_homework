1f72cbf4c561b9ed28e258fabbd290b5f203cbfa
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-13 00:04:48 +0500

    20 misol

--- logic20.py
+++ logic20.py
@@ -1,5 +1,7 @@
 def main(n):
-    """
+    return n%10 and n>0
+print(main(11100114))
+"""
     A number consisting of one and zero is given (the number starts at once). 
     If the number of ones is greater than zero, true, otherwise False is returned.
     
@@ -8,5 +10,3 @@ def main(n):
     Returns:
         bool: answer
     """
-
-    return
