2392ea9cd0b7259ac52ee26bd42d0d49474622e2
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 23:24:07 +0500

    14 misol

--- logic14.py
+++ logic14.py
@@ -1,9 +1,10 @@
 def main(a):
-    """
+    return a//10 + a%10 and a%2==1
+print(main(25))
+"""
     Given a two-digit integer a,  check the following statement "All digits sum is odd".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
