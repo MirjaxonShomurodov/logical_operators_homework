fa89c1886bc93631ff964b9f9555a31854159964
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 21:13:09 +0500

    5 misol

--- logic05.py
+++ logic05.py
@@ -1,5 +1,7 @@
 def main(a,b):
-    """
+    return a%2==1 and b%2==1 
+print(main(3,7))
+"""
     Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is odd".
     Args:
         a(int): parameter a
@@ -7,4 +9,3 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
