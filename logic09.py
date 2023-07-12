46ab9b38bbb56edcd5a1fd8affae4023ef9ebba3
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-12 22:43:34 +0500

    9 milol

--- logic09.py
+++ logic09.py
@@ -1,5 +1,7 @@
 def main(a,b):
-    """
+    return (a%2==1 and b%2==0) or (b%2==1 and a%2==0)
+print(main(7,3))
+"""
     Given two integers a, b,  check the following statement "At least one of the numbers 'a' and 'b' is odd".
     Args:
         a(int): parameter a
@@ -7,4 +9,3 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
