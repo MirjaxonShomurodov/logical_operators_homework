f8ccac40957de0d5afcf62c41b65bbe924b4577a
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-13 09:48:09 +0500

    15 misol

--- logic15.py
+++ logic15.py
@@ -1,9 +1,10 @@
 def main(a):
-    """
+    return (a//100 + a//10%10 + a%10)%2==1
+print(main(535))
+"""
     Given a three-digit integer a,  check the following statement "All digits sum is odd".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
