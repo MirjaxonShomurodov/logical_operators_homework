05e4267856a47b3ab9b3c9e54728bcfa6b0bec90
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-13 09:56:22 +0500

    19 misol

--- logic19.py
+++ logic19.py
@@ -1,5 +1,7 @@
 def main(x):
-    """
+    return x//10==x%10
+print(main(11))
+"""
     Given a two digit integer x, return true if x is palindrome integer.
     An integer is a palindrome, when it reads the same backward as forward.
 
@@ -8,4 +10,3 @@ def main(x):
     Returns:
         bool: answer
     """
-    return 
