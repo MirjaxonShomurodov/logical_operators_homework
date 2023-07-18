905857c908b6801b82661f234449dd5af954d29a
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-18 12:49:19 +0500

    20 misol

--- logic20.py
+++ logic20.py
@@ -1,12 +1,29 @@
-def main(n):
-    return n%10 and n>0
-print(main(11100114))
-"""
-    A number consisting of one and zero is given (the number starts at once). 
-    If the number of ones is greater than zero, true, otherwise False is returned.
+# def main(n):
+ 
+# """
+#     A number consisting of one and zero is given (the number starts at once). 
+#     If the number of ones is greater than zero, true, otherwise False is returned.
     
-    Args:
-        n(int): parameter n
-    Returns:
-        bool: answer
-    """
+#     Args:
+#         n(int): parameter n
+#     Returns:
+#         bool: answer
+#     """
+def main(n):
+    n1=n%10
+    n//=10
+
+    n2=n%10
+    n//=10
+
+    n3=n%10
+    n//=10
+
+    n4=n%10
+    n//=10
+    n5=n
+    son=n1+n2+n3+n4+n5
+    son1=5-son
+    return son>son1
+n=11110
+print(main(n))
